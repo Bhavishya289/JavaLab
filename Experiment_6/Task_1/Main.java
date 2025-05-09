638e366e556ab75ae412bf166379e04dd5dd0e84
Bhavishya Singh <arabhavishyasingh2006@gmail.com>
2025-05-09 23:02:23 +0530

    Delete Experiment_6 directory

--- Experiment_6/Task_1/Main.java
+++
@@ -1,8 +0,0 @@
-import Balance.Account;  
-
-public class Main {
-    public static void main(String[] args) {
-        Account myAccount = new Account(9000.90); //init object
-        myAccount.Display_Balance();  
-    }
-}
--- Experiment_6/Task_1/tempCodeRunnerFile.java
+++
@@ -1,2 +0,0 @@
-   myAccount.Display_Balance();  
-    }
